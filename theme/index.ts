ed7ce2c0cff8667d6e4ad89914328ce7d2380757
ascension <jerroddb@gmail.com>
2021-10-13 19:25:42 -0600

    move theme and add to provider

--- theme/index.ts
+++ theme/index.ts
@@ -1,5 +1,4 @@
 import { extendTheme, ThemeConfig } from "@chakra-ui/react";
-import { mode } from "@chakra-ui/theme-tools";
 
 const config: ThemeConfig = {
   initialColorMode: "dark",
@@ -8,14 +7,6 @@ const config: ThemeConfig = {
 
 const theme = extendTheme({
   config,
-  styles: {
-    global: (props) => ({
-      body: {
-        color: mode("grey.800", "black")(props),
-        bg: mode("white", "#080A0C")(props),
-      },
-    }),
-  },
 });
 
 export default theme;
